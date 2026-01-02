afc0eb44abc430218d8bb7486c8798ea87ade2a2
kuanhaolin <linkuanhao.0924@gmail.com>
2026-01-03 07:30:35 +0800

    Test decryption by enumeration

--- oracle/keygen.js
+++ oracle/keygen.js
@@ -1,31 +1,32 @@
 const { buildBabyjub } = require("circomlibjs");
+const crypto = require("crypto");
 
 async function main() {
     const babyJub = await buildBabyjub();
     const F = babyJub.F;
     const G = babyJub.Base8;
     
-    // secret keys
-    const sk1 = BigInt(Math.floor(Math.random() * 10**20));
-    const sk2 = BigInt(Math.floor(Math.random() * 10**20));
-    const sk3 = BigInt(Math.floor(Math.random() * 10**20));
+    // generate secret keys
+    function getSecureRandomBigInt() {
+        const buf = crypto.randomBytes(10);
+        return BigInt("0x" + buf.toString("hex"));
+    }
+    const sk1 = getSecureRandomBigInt();
+    const sk2 = getSecureRandomBigInt();
+    const sk3 = getSecureRandomBigInt();
     const skTotal = sk1 + sk2 + sk3;
 
     // public key
-    const pk = babyJub.mulPointEscalar([G[0], G[1]], skTotal);
-
+    const pk = babyJub.mulPointEscalar(G, skTotal);
     // show keys
     console.log("sk01:", sk1.toString());
     console.log("sk02:", sk2.toString());
     console.log("sk03:", sk3.toString());
+    console.log("skTotal:", skTotal.toString());
     console.log("pkX:", F.toObject(pk[0]).toString());
     console.log("pkY:", F.toObject(pk[1]).toString());
     console.log("gX:", F.toObject(G[0]).toString());
     console.log("gY:", F.toObject(G[1]).toString());
 }
 
-
-main().catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+main().catch(console.error);
